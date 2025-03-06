3d387a8ce14a0e4b9202aa2149de85e92634bfd6
Alfonsus <axeolume@gmail.com>
2025-03-06 23:01:42 +0700

    asd

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/test-joki-overlay', // Ganti dengan nama repo
+  base: '/test-joki-overlay-new', // Ganti dengan nama repo
 });
